790eef73679fedcc9a4e02668006af3812eadca8
massaracsh7 <fatum7@gmail.com>
2024-08-19 17:54:35 +0300

    fix: fix pathes

--- src/modules/Footer.ts
+++ src/modules/Footer.ts
@@ -6,10 +6,10 @@ import Element from './Element';
 export default class Footer implements PartsPage {
   view = (): HTMLElement => {
     const logoImg = document.createElement('img');
-    logoImg.src = 'https://rs.school/images/rs_school_js.svg';
+    logoImg.src = './images/rs_school_js.svg';
     const linkGit = new Link('footer__git', 'github', 'https://github.com/massaracsh7');
     const copy = new Element('div', 'footer__year', '2023');
-    const linkRss = new Link('footer__rss', '', 'https://rs.school/js/').create();
+    const linkRss = new Link('footer__rss', '', 'https://rs.school/').create();
     linkRss.append(logoImg);
     const footer = new Block('footer', 'footer', linkGit.create(), copy.create(), linkRss);
     return footer.create();
